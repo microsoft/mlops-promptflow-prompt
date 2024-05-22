8cc58b52f18888dbbc74016d7bcffd4c412fca10
Sergii Baidachnyi <sbaydach@microsoft.com>
2024-05-22 14:16:02 -0700

    fix parameters

--- src/basic_func_impl/function_basic_invoke/__init__.py
+++ src/basic_func_impl/function_basic_invoke/__init__.py
@@ -1,20 +1,17 @@
 import logging
-import os
 import azure.functions as func
-from dotenv import load_dotenv
 from .flow_code.extract_entities import extract_entity
 
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    load_dotenv()
     entity_type = req.params.get('entity_type')
     text = req.params.get('text')
 
     if entity_type and text:
 
-        result = os.environ.get("AZURE_OPENAI_ENDPOINT") #extract_entity(entity_type=entity_type, text=text)
+        result = extract_entity(entity_type=entity_type, text=text)
 
         return func.HttpResponse(f"{result}", status_code=200)
     else:
