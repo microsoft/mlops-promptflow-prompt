fb59a8ca7c61cfd39e3a3324d69912268d3232bb
Oleksandr <oteslenkoua@gmail.com>
2024-05-23 13:29:13 +0100

    initial eval of chat_with_pdf

---
+++ flows/chat_with_pdf/evaluate/evaluate.py
@@ -0,0 +1,64 @@
+"""Evaluation flow for yaml_basic_flow."""
+import argparse
+from pprint import pprint
+from promptflow.evals.evaluate import evaluate
+from mlops.common.config_utils import MLOpsConfig
+from src.evaluators.match_evaluator import MatchEvaluator
+from flows.chat_with_pdf.evaluate.flow_wrapper import StandardFlowWrapper
+from mlops.common.naming_tools import generate_experiment_name
+
+
+def main():
+    """Implement parameter reading and evaluation flow."""
+    # Config parameters
+    parser = argparse.ArgumentParser("config_parameters")
+    parser.add_argument(
+        "--environment_name",
+        type=str,
+        required=True,
+        help="env_name from config.yaml",
+    )
+    args = parser.parse_args()
+
+    mlops_config = MLOpsConfig(environemnt=args.environment_name)
+    flow_config = mlops_config.get_flow_config(flow_name="yaml_chat_with_pdf")
+
+    matchevaluator = MatchEvaluator()
+
+    data_eval_path = flow_config['eval_data_path']
+    print("data_eval_path:", data_eval_path)
+    flow_standard_path = flow_config["standard_flow_path"]
+    print("flow_standard_path:", flow_standard_path)
+    aoai_deployment = flow_config["CHAT_MODEL_DEPLOYMENT_NAME"]
+    print("aoai_deployment:", aoai_deployment)
+
+    aistudio_config = mlops_config.aistudio_config
+    openai_config = mlops_config.aoai_config
+
+    flow = StandardFlowWrapper(flow_standard_path, flow_config["connection_name"], aoai_deployment, openai_config)
+
+    results = evaluate(
+        evaluation_name=generate_experiment_name("yaml_chat_with_pdf"),
+        data=data_eval_path,
+        target=flow,
+        evaluators={
+            "matchevaluator": matchevaluator,
+        },
+        evaluator_config={
+            "matchevaluator": {
+                "response": "${target.answer}",
+                "ground_truth": "${data.groundtruth}"
+            },
+        },
+        azure_ai_project={
+            "subscription_id": aistudio_config["subscription_id"],
+            "resource_group_name": aistudio_config["resource_group_name"],
+            "project_name": aistudio_config["project_name"]
+        }
+    )
+
+    pprint(results)
+
+
+if __name__ == "__main__":
+    main()
---
+++ flows/chat_with_pdf/evaluate/flow_wrapper.py
@@ -0,0 +1,31 @@
+"""Implement a wrapper for yaml based flow since it's not supported by evaluate explicitly."""
+from promptflow.client import load_flow
+from promptflow.entities import FlowContext
+from promptflow.entities import AzureOpenAIConnection
+from promptflow.client import PFClient
+
+
+class StandardFlowWrapper:
+    """Implement the flow."""
+
+    def __init__(self, flow_standard_path: str, connection_name: str, aoai_deployment: str, aoai_config: dict):
+        """Initialize environment and load prompty into the memory."""
+        connection = AzureOpenAIConnection(
+            name=connection_name,
+            api_key=aoai_config["aoai_api_key"],
+            api_base=aoai_config["aoai_api_base"],
+            api_type="azure",
+            api_version=aoai_config["aoai_api_version"],
+        )
+
+        pf = PFClient()
+        pf.connections.create_or_update(connection)
+
+        self.flow = load_flow(flow_standard_path)
+        self.flow.context = FlowContext(
+            connections={"setup_env": {"connection": connection}}
+        )
+
+    def __call__(self, *, chat_history: str, pdf_url: str, question: str, **kwargs):
+        """Invoke the flow for a single request."""
+        return self.flow(chat_history=chat_history, pdf_url=pdf_url, question=question)
