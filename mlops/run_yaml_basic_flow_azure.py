3354c5cf8c9c2bcd316f14d3eb875d31b95bb6cd
Anastasiya Gromova <anastasiya.gromova.contractor@pepsico.com>
2024-05-28 18:36:23 -0700

    implemented verification for the Azure OpenAI connection

--- mlops/run_yaml_basic_flow_azure.py
+++ mlops/run_yaml_basic_flow_azure.py
@@ -3,7 +3,9 @@ import argparse
 from promptflow.azure import PFClient
 from mlops.common.config_utils import MLOpsConfig
 from azure.identity import DefaultAzureCredential
-
+from azure.ai.ml.entities import (Hub, Project, AzureOpenAIConnection, AzureAISearchConnection)
+from azure.ai.ml import MLClient
+import logging
 
 def main():
     """
@@ -29,6 +31,20 @@ def main():
     # Run the flow as a function.
     flow_standard_path = flow_config["standard_flow_path"]
 
+ # Azure OpenAI Connection check (aoai):
+    try:
+        ml_client = MLClient(
+            subscription_id=aistudio_config['subscription_id'],
+            resource_group_name=aistudio_config['resource_group_name'],
+            workspace_name=aistudio_config['project_name'],
+            credential=DefaultAzureCredential(),
+        )
+        created_connection = ml_client.connections.get(flow_config["connection_name"])
+        logging.debug(f"connection found: {created_connection}")
+    except Exception as e:
+        logging.debug(f"connection not found: {e}")
+        raise Exception("Azure OpenAI configuration connection does not exist.")
+
     credential = DefaultAzureCredential()
     pf = PFClient(
         credential,
@@ -47,8 +63,13 @@ def main():
         #connections={"NER_LLM": {"connection": flow_config["connection_name"], "deployment_name": aoai_deployment}}
     )
     pf.stream(run_instance)
+
+    print(f"Current status is: {run_instance.status}")
+
     if run_instance.status == "Completed" or run_instance.status == "Finished":
         print("Experiment has been completed")
+    elif run_instance.status =="Preparing":
+        print("Preparing flow run")
     else:
         raise Exception("Sorry, exiting job with failure..")
     print(run_instance.name)
