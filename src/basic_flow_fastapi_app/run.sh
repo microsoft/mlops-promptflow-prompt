5edf58a7af42b2db379ebfa5d9e082a627879283
joshuaphelpsms <joshuaphelps@microsoft.com>
2024-06-13 22:58:21 +0000

    add otel collector and auto instrumentation

---
+++ src/basic_flow_fastapi_app/run.sh
@@ -0,0 +1,16 @@
+PORT=${PORT:-8080}
+HOST=${HOST:-"0.0.0.0"}
+OTEL_SERVICE_NAME=${OTEL_SERVICE_NAME:-"promptflow.fastapi"}
+OTEL_EXPORTER_OTLP_ENDPOINT=${OTEL_EXPORTER_OTLP_ENDPOINT:-"http://otel-collector-collector.default.svc.cluster.local:4317"}
+
+export OTEL_RESOURCE_ATTRIBUTES="service.version=${OTEL_SERVICE_VERSION},service.instance.id=${OTEL_SERVICE_INSTANCE_ID}"
+
+export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
+
+opentelemetry-instrument \
+    --traces_exporter otlp \
+    --metrics_exporter otlp \
+    --logs_exporter otlp \
+    --service_name "{$OTEL_SERVICE_NAME}" \
+    --exporter_otlp_endpoint "${OTEL_EXPORTER_OTLP_ENDPOINT}" \
+    uvicorn main:app --host "${HOST}" --port "${PORT}"
