0d7ff7b3aa22570678fad818dc7c3fefd0743075
joshuaphelpsms <joshuaphelps@microsoft.com>
2024-06-13 23:03:31 +0000

    add service version and instance id

--- src/basic_flow_fastapi_app/run.sh
+++ src/basic_flow_fastapi_app/run.sh
@@ -1,6 +1,9 @@
 PORT=${PORT:-8080}
 HOST=${HOST:-"0.0.0.0"}
 OTEL_SERVICE_NAME=${OTEL_SERVICE_NAME:-"promptflow.fastapi"}
+OTEL_SERVICE_VERSION=${OTEL_SERVICE_VERSION:-"0.0.1"}
+uuid=$(python -c "import uuid; print(uuid.uuid4())")
+OTEL_SERVICE_INSTANCE_ID=${OTEL_SERVICE_INSTANCE_ID:-${uuid}}
 OTEL_EXPORTER_OTLP_ENDPOINT=${OTEL_EXPORTER_OTLP_ENDPOINT:-"http://otel-collector-collector.default.svc.cluster.local:4317"}
 
 export OTEL_RESOURCE_ATTRIBUTES="service.version=${OTEL_SERVICE_VERSION},service.instance.id=${OTEL_SERVICE_INSTANCE_ID}"
