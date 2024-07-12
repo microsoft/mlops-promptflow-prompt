1661f18937fd55a5be324606f213fc403c3e825f
Oleksandr <oteslenkoua@gmail.com>
2024-07-12 19:44:11 +0100

    build stroage url from storage name

--- flows/chat_with_pdf/standard/chat_with_pdf/constants.py
+++ flows/chat_with_pdf/standard/chat_with_pdf/constants.py
@@ -3,6 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STORAGE_ACCOUNT_URL = os.getenv("STORAGE_ACCOUNT_URL")
+
+STORAGE_ACCOUNT_URL = f"https://{os.getenv('STORAGE_ACCT_NAME')}.blob.core.windows.net/"
 PDFS_CONTAINER_NAME = "pdf-container"
 INDEX_CONTAINER_NAME = "faiss-index"
